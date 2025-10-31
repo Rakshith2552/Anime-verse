42e7b40b69a32d0f12c4b8e02e30deddc179211c
Rakshith <rakshith0525@gmail.com>
2025-10-31 21:16:25 +0530

    updated project home,base,details

--- animeapp/urls.py
+++ animeapp/urls.py
@@ -3,4 +3,5 @@ from animeapp import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('anime/<int:anime_id>/',views.anime_details,name='anime_detail'),
 ]
--- animeapp/views.py
+++ animeapp/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    query=request.GET.get('q')
+    results=[]
+    
+    if query:
+        url=f'https://api.jikan.moe/v4/anime?q={query}&limit=20'
+        response=requests.get(url)
+        if response.status_code==200:
+            data=response.json()
+            results=data.get('data',[])
+    return render(request,'home.html',{'results':results,'query':query})
+
+
+def anime_details(request,anime_id):
+    url=f'https://api.jikan.moe/v4/anime/{anime_id}/full'
+    response=requests.get(url)
+    anime=None
+    
+    if response.status_code==200:
+        data=response.json()
+        anime=data.get('data',None)
+    else:
+        print("Error fetching anime details")
+    
+    return render(request,'anime_detail.html',{'anime':anime})
